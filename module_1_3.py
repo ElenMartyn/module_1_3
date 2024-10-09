b8a2b59cb7b3bd74d5c8bd15e824fe788c08faf3
ElenMartyn <elenamartyn00@gmail.com>
2024-10-09 21:11:56 +0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Elena'
+print (name)
+age = 24
+print(age)
+print(age + 1)
+is_student = True
+print( is_student)
+
+print('Name:' + name)
+print ('Age:' + str(age))
+print('New Age:' + str(int(age + 1)))
+print('Is Student:' + str(bool(is_student)))
